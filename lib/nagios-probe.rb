4e3a99d913c43492aff59e5931ad8a289f2e86bc
Kevin M. Dulzo <kdulzo@me.com>
2010-10-18 20:53:05 -0500

    Refactor module functions and kill the module include.
    No more Nagios::Probe::Probe::Probe.

--- lib/nagios-probe.rb
+++ lib/nagios-probe.rb
@@ -4,49 +4,36 @@ module Nagios
   CRITICAL = 2
   UNKNOWN = 3
 
-  def crit(message)
-    "Critical: #{message}"
-  end
-
-  def warn(message)
-    "Warning: #{message}"
-  end
-
-  def ok(message)
-    "OK: #{message}"
-  end
-
   class Probe
-    include Nagios
     attr_reader :retval, :message
 
     def initialize(opts = {})
       @opts = opts
-      @retval = OK
-      @message = ok(ok_message)
+      @retval = Nagios::OK
+      @message = "OK: #{ok_message}"
     end
 
     def crit?
       is_crit = check_crit
       return false unless is_crit
-      @retval = CRITICAL
-      @message = crit(crit_message)
+      @retval = Nagios::CRITICAL
+      @message = "Critical: #{crit_message}"
       true
     end
 
     def warn?
       is_warn = check_warn
       return false unless is_warn
-      @retval = WARNING
-      @message = warn(warn_message)
+      @retval = Nagios::WARNING
+      @message = "Warning: #{warn_message}"
       true
     end
 
     def ok?
       is_ok = check_ok
       return false unless is_ok
-      @retval = OK
-      @message = ok(ok_message)
+      @retval = Nagios::OK
+      @message = "OK: #{ok_message}"
       true
     end
 
@@ -58,7 +45,7 @@ module Nagios
       if !crit?
         if !warn?
           if !ok?
-            @retval = UNKNOWN
+            @retval = Nagios::UNKNOWN
             raise RuntimeError, "crit? warn? and ok? all returned false"
           end
         end
